360cf18601f2a131478a813f2e49859bccf23c02
wenglesUx <85462594+wenglesUx@users.noreply.github.com>
2023-02-16 17:40:30 -0300

    atualização script

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
+const apikey ="fda25bf436c27c8c55023d92a494b9ea"
+const apiCountryURL ="https://countryflagsapi.com/png/"
+
+const cityinput = document.querySelector('#city-input');
+const serachbtn = document.querySelector('#search');
+
+
+const cityElement = document.querySelector('#city')
+const tempElement = document.querySelector('#temperature span')
+const descElement = document.querySelector('#description')
+const weatherElement = document.querySelector('#weather-icon')
+const umidityElement = document.querySelector('#umidity span')
+const countryElement  = document.querySelector('#country')
+const windElement = document.querySelector('#wind span')
+
+
+
+const getweatherdata = async(city) =>{
+     const apiweatherURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apikey}&lang=pt_br`;
+
+     const res = await fetch(apiweatherURL)
+     const data = await res.json()
+
+     return data
+}
+const showweatherdata = async (city) => {
+     const data = await getweatherdata(city);
+
+          cityElement.innerHTML=data.name
+          tempElement.innerHTML=parseInt(data.main.temp)
+          descElement.innerHTML=data.weather[0].description
+          weatherElement.innerHTML=setAttribute("src",`https://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+          umidityElement.innerHTML=`${data.humidity}%`
+          countryElement.innerHTML=setAttribute("src", apiCountryURL + data.sys.country)
+          windElement.innerHTML=`${data.wind.speed}`
+
+          console.log(city)
+        
+}
+
+serachbtn.addEventListener('click',(e)=>{
+
+     e.preventDefault()
+     const city = cityinput.value;
+
+     showweatherdata(city)
+})
+
+
+/*
 const a =(el)=>document.querySelector(el)
 
 var button =a('.button')
@@ -10,7 +60,7 @@ var main = a('.main')
 button.addEventListener('click', function(){
 
 
-     fetch('http://api.openweathermap.org/data/2.5/weather?q='+'luziania'+'&limit=1&appid=fda25bf436c27c8c55023d92a494b9ea ')
+     fetch('http://api.openweathermap.org/data/2.5/weather?q='+inputvalue.value+'&limit=1&appid=fda25bf436c27c8c55023d92a494b9ea ')
      .then(Response => Response.json())
      .then(data => {
        
@@ -39,3 +89,4 @@ button.addEventListener('click', function(){
 
 
 })
+*/
